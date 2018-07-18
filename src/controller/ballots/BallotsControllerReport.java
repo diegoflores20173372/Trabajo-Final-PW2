3bcbf62d8497d588bf24de97b6c33049511549bc
Diego Flores <dflorescam@unsa.edu.pe>
2018-07-18 10:57:14 -0500

    Proyecto Final de PW2

---
+++ src/controller/ballots/BallotsControllerReport.java
@@ -0,0 +1,60 @@
+package controller.ballots;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.UserServiceFactory;
+
+import javax.jdo.PersistenceManager;
+import model.entity.*;
+@SuppressWarnings("serial")
+public class BallotsControllerReport extends HttpServlet{
+	public static int NRecibo = 1;
+	@SuppressWarnings("unchecked")
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException{	
+		com.google.appengine.api.users.User cuentaGoogle = UserServiceFactory.getUserService().getCurrentUser();
+		if(cuentaGoogle == null){
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/Views/Errors/deny1.jsp");
+			dispatcher.forward(request, response);
+		}
+		else{
+			PersistenceManager pm = PMF.get().getPersistenceManager();
+			String query = "select from " + model.entity.User.class.getName() + " where email=='" + cuentaGoogle.getEmail() + "'" + "&& status==true";
+			List<model.entity.User> uSearch = (List<model.entity.User>) pm.newQuery(query).execute();
+			if(uSearch.isEmpty()){
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/Views/Errors/deny2.jsp");
+				dispatcher.forward(request, response);
+			}
+			else{
+				System.out.println(request.getServletPath());
+				String query2 = "select from " + Resources.class.getName() + " where url=='" + request.getServletPath() + "'" + " && status==true";
+				List<Resources> rSearch = (List<Resources>) pm.newQuery(query2).execute();
+				if(rSearch.isEmpty()){
+					RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/Views/Errors/deny3.jsp");
+					dispatcher.forward(request, response);
+				}
+				else{
+					String query3 = "select from " + model.entity.Access.class.getName() + " where idRol==" + uSearch.get(0).getIdRol() +
+							"&& idUrl==" + rSearch.get(0).getId() + "&& status==true";
+					List<model.entity.Access> aSearch = (List<model.entity.Access>) pm.newQuery(query3).execute();
+					if(aSearch.isEmpty()){
+						RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/Views/Errors/deny4.jsp");
+						dispatcher.forward(request, response);
+					}
+					else{
+						query3 ="select from " + Ballot.class.getName();
+						List<Ballot> miList = (List<Ballot>) pm.newQuery(query3).execute();
+						
+					}
+				}
+			}
+		}
+	}
+}
